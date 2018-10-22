119de789cfb2e4f63a4416e9b687e75c23a2c8b8
Hamish Lees <hamishlees6@gmail.com>
2018-10-22 15:17:44 +1100

    initial commit for new branch

--- lib/cryptocurrency.rb
+++ lib/cryptocurrency.rb
@@ -17,6 +17,24 @@ module Cryptocurrency
     def tracking
     end
 
+    def search(search_term)
+        response = HTTP.get('https://api.coinmarketcap.com/v2/ticker/')
+        parsed_response = JSON.parse(response.body)["data"]
+        sorted_response = parsed_response["data"]
+        sorted_response.each {|k,v| 
+          if v["name"] == search_term
+            data = [
+                v["name"], 
+                v["symbol"], 
+                v["price"],
+                number_to_percentage((v["quotes"]["USD"]["percent_change_24h"]), precision: 2),
+                number_to_currency(v["quotes"]["USD"]["volume_24h"])
+            ]
+          end
+        }
+        data
+    end
+
     private
     def gainersLosers
         response = HTTP.get('https://api.coinmarketcap.com/v2/ticker/?sort=volume_24&limit=100')
